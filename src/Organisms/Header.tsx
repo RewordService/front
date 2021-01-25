19926f0c42bee783857a034705b710e7c48bc7a2
izszzz <izszzz@.yahoo.co.jp>
2021-01-26 04:08:49 +0900

    update App to ts

--- src/Organisms/Header.tsx
+++ src/Organisms/Header.tsx
@@ -4,13 +4,17 @@ import {createStyles, makeStyles, Theme} from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
-import LabelImportantIcon from "@material-ui/icons/LabelImportant"
 import Link from "@material-ui/core/Link"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
-import SearchIcon from "@material-ui/icons/Search"
 import Grid from "@material-ui/core/Grid"
 import Box from "@material-ui/core/Box"
+import Hidden from "@material-ui/core/Hidden"
+import IconButton from "@material-ui/core/IconButton"
+import Drawer from "@material-ui/core/Drawer"
+import LabelImportantIcon from "@material-ui/icons/LabelImportant"
+import SearchIcon from "@material-ui/icons/Search"
+import MenuIcon from "@material-ui/icons/Menu"
 //scripts
 import {IsSignedIn} from "../Axios/UsersController"
 //partials
@@ -40,75 +44,85 @@ function Header() {
     setSearch(e.target.value)
 
   return (
-    <AppBar color="default">
-      <Toolbar>
-        <LabelImportantIcon />
-        <Typography variant="h5">
-          <Link
-            color="inherit"
-            underline="none"
-            component={RouterLink}
-            to={IsSignedIn() ? routes.HOME : routes.LANDING}
-          >
-            REWORD
-          </Link>
-        </Typography>
-        <Box className={classes.root}>
-          <Button component={RouterLink} to={routes.HOME}>
-            Home
-          </Button>
-          <Button component={RouterLink} to={routes.GAME}>
-            Game
-          </Button>
-          {IsSignedIn() ? (
-            <Button
-              color="primary"
-              variant="contained"
+    <>
+      <AppBar color="default">
+        <Toolbar>
+          <Hidden mdUp>
+            <IconButton color="inherit">
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
+          <LabelImportantIcon />
+          <Typography variant="h5">
+            <Link
+              color="inherit"
+              underline="none"
               component={RouterLink}
-              to={routes.SIGNOUT}
-              disableElevation
+              to={IsSignedIn() ? routes.HOME : routes.LANDING}
             >
-              SignOut
-            </Button>
-          ) : (
-            <>
-              <Button
-                color="primary"
-                variant="contained"
-                component={RouterLink}
-                to={routes.SIGNIN}
-                disableElevation
-              >
-                SignIn
+              REWORD
+            </Link>
+          </Typography>
+          <Hidden mdDown>
+            <Box className={classes.root}>
+              <Button component={RouterLink} to={routes.HOME}>
+                Home
               </Button>
-              <Button
-                color="primary"
-                variant="contained"
-                component={RouterLink}
-                to={routes.SIGNUP}
-                disableElevation
-              >
-                SignUp
+              <Button component={RouterLink} to={routes.GAME}>
+                Game
               </Button>
-            </>
-          )}
-        </Box>
-        <Box ml="auto">
-          <Grid container alignItems="flex-end">
-            <Grid item>
-              <SearchIcon />
+              {IsSignedIn() ? (
+                <Button
+                  color="primary"
+                  variant="contained"
+                  component={RouterLink}
+                  to={routes.SIGNOUT}
+                  disableElevation
+                >
+                  SignOut
+                </Button>
+              ) : (
+                <>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    component={RouterLink}
+                    to={routes.SIGNIN}
+                    disableElevation
+                  >
+                    SignIn
+                  </Button>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    component={RouterLink}
+                    to={routes.SIGNUP}
+                    disableElevation
+                  >
+                    SignUp
+                  </Button>
+                </>
+              )}
+            </Box>
+          </Hidden>
+          <Box ml="auto">
+            <Grid container alignItems="flex-end">
+              <Grid item>
+                <SearchIcon />
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="Search User"
+                  onKeyDown={SearchResult}
+                  onChange={handleChange}
+                />
+              </Grid>
             </Grid>
-            <Grid item>
-              <TextField
-                label="Search User"
-                onKeyDown={SearchResult}
-                onChange={handleChange}
-              />
-            </Grid>
-          </Grid>
-        </Box>
-      </Toolbar>
-    </AppBar>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer />
+    </>
   )
 }
 
--- src/Pages/LP.tsx
+++ src/Pages/LP.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import {Link} from "react-router-dom"
+/* @ts-ignore */
+import Fade from "react-reveal/Fade"
 import Box from "@material-ui/core/Box"
 import Typography from "@material-ui/core/Typography"
 import Container from "@material-ui/core/Container"
@@ -15,106 +17,126 @@ import routes from "../constants/routes.json"
 export default function LP() {
   return (
     <>
-      <FirstBox display="flex" alignItems="center" height="100vh">
-        <Container>
-          <BoldTypography variant="h2" color="inherit">
-            Reword
-          </BoldTypography>
-          <BoldTypography variant="h3" color="inherit">
-            -ワーキングメモリーを鍛える-
-          </BoldTypography>
-          <BoldTypography variant="body1" color="inherit" gutterBottom>
-            逆唱を行うことでワーキングメモリーを鍛えます。
-            <br />
-            会員登録を行うことで、試行回数、正答率などを記録し
-            <br />
-            グラフで確認することができます
-          </BoldTypography>
-          <AuthBtns />
-        </Container>
-      </FirstBox>
-
-      <Box height="100vh" display="flex" alignItems="center">
-        <Container>
-          <Box mb={8}>
-            <BoldTypography variant="h3" align="center">
-              ワーキングメモリーとは?
+      <Fade>
+        <FirstBox display="flex" alignItems="center" height="100vh">
+          <Container>
+            <BoldTypography variant="h2" color="inherit">
+              <Fade left cascade>
+                Reword
+              </Fade>
             </BoldTypography>
-          </Box>
-          <BoldTypography variant="body1" align="center" gutterBottom>
-            ワーキングメモリーとは
-            <span style={{color: "red"}}>
-              「短期記憶に存在する情報に対して処理を行う能力」
-            </span>
-            を指します。
-          </BoldTypography>
-          <BoldTypography variant="body1" align="center" gutterBottom>
-            <br /> Rewordでは、
-            <span style={{color: "blue"}}>
-              「短期記憶に保持した文字列を逆から読み返すという処理」
-            </span>
-            を行います。
-          </BoldTypography>
-        </Container>
-      </Box>
-
-      <Box
-        style={{background: "#2b2b2b", color: "white"}}
-        display="flex"
-        alignItems="center"
-        height="100vh"
-      >
-        <Container>
-          <Box mb={5}>
-            <BoldTypography variant="h3" color="inherit" align="center">
-              Reword
+            <BoldTypography variant="h3" color="inherit">
+              <Fade left cascade>
+                -ワーキングメモリーを鍛える-
+              </Fade>
             </BoldTypography>
-            <BoldTypography variant="body1" color="inherit" align="center">
-              Rewordではワーキングメモリーを鍛えるサービスを提供します。
+            <BoldTypography variant="body1" color="inherit" gutterBottom>
+              逆唱を行うことでワーキングメモリーを鍛えます。
+              <br />
+              会員登録を行うことで、試行回数、正答率などを記録し
+              <br />
+              グラフで確認することができます
             </BoldTypography>
-          </Box>
-          <Grid container spacing={5}>
-            <Grid item xs={12} lg={4}>
-              <Box fontSize="60px" textAlign="center">
-                <ForumIcon fontSize="inherit" />
-              </Box>
-              <BoldTypography variant="h5" align="center" gutterBottom>
-                - 逆唱で鍛える -
+            <AuthBtns />
+          </Container>
+        </FirstBox>
+      </Fade>
+      <Fade>
+        <Box height="100vh" display="flex" alignItems="center">
+          <Container>
+            <Box mb={8}>
+              <Fade bottom>
+                <BoldTypography variant="h3" align="center">
+                  ワーキングメモリーとは?
+                </BoldTypography>
+              </Fade>
+            </Box>
+            <BoldTypography variant="body1" align="center" gutterBottom>
+              ワーキングメモリーとは
+              <span style={{color: "red"}}>
+                「短期記憶に存在する情報に対して処理を行う能力」
+              </span>
+              を指します。
+            </BoldTypography>
+            <BoldTypography variant="body1" align="center" gutterBottom>
+              <br /> Rewordでは、
+              <span style={{color: "blue"}}>
+                「短期記憶に保持した文字列を逆から読み返すという処理」
+              </span>
+              を行います。
+            </BoldTypography>
+          </Container>
+        </Box>
+      </Fade>
+      <Fade>
+        <Box
+          style={{background: "#2b2b2b", color: "white"}}
+          display="flex"
+          alignItems="center"
+          minHeight="100vh"
+        >
+          <Container>
+            <Box mb={10}>
+              <BoldTypography variant="h3" color="inherit" align="center">
+                <Fade left cascade>
+                  Reword
+                </Fade>
               </BoldTypography>
-              <Typography variant="body1" color="inherit">
-                逆唱とは、ある文字列を提示された際、その文字列を反対から読み返すことを指します。
-                <br />
-                この処理を少しづつ負荷をかけながら継続をすることで鍛えることができます。
-              </Typography>
-            </Grid>
+              <BoldTypography variant="body1" color="inherit" align="center">
+                <Fade left cascade>
+                  Rewordではワーキングメモリーを鍛えるサービスを提供します。
+                </Fade>
+              </BoldTypography>
+            </Box>
+            <Grid container spacing={8}>
+              <Grid item xs={12} lg={4}>
+                <Fade duration={1000} bottom>
+                  <Box fontSize="60px" textAlign="center">
+                    <ForumIcon fontSize="inherit" />
+                  </Box>
+                  <BoldTypography variant="h5" align="center" gutterBottom>
+                    - 逆唱で鍛える -
+                  </BoldTypography>
+                  <Typography variant="body1" color="inherit" align="center">
+                    逆唱とは、ある文字列を提示された際、その文字列を反対から読み返すことを指します。
+                    <br />
+                    この処理を少しづつ負荷をかけながら継続をすることで鍛えることができます。
+                  </Typography>
+                </Fade>
+              </Grid>
 
-            <Grid item xs={12} lg={4}>
-              <Box fontSize="60px" textAlign="center">
-                <BarChartIcon fontSize="inherit" />
-              </Box>
-              <BoldTypography variant="h5" align="center" gutterBottom>
-                - 成績をグラフ化 -
-              </BoldTypography>
-              <Typography variant="body1" color="inherit">
-                会員登録を行うことで、成績をグラフ化し自分の成長を可視化することができます。
-              </Typography>
-            </Grid>
+              <Grid item xs={12} lg={4}>
+                <Fade duration={2000} bottom>
+                  <Box fontSize="60px" textAlign="center">
+                    <BarChartIcon fontSize="inherit" />
+                  </Box>
+                  <BoldTypography variant="h5" align="center" gutterBottom>
+                    - 成績をグラフ化 -
+                  </BoldTypography>
+                  <Typography variant="body1" color="inherit" align="center">
+                    会員登録を行うことで、成績をグラフ化し自分の成長を可視化することができます。
+                  </Typography>
+                </Fade>
+              </Grid>
 
-            <Grid item xs={12} lg={4}>
-              <Box fontSize="60px" textAlign="center">
-                <StorageIcon fontSize="inherit" />
-              </Box>
-              <BoldTypography variant="h5" align="center" gutterBottom>
-                - データの蓄積 -
-              </BoldTypography>
-              <Typography variant="body1" color="inherit">
-                正答数や正答率、試行回数、の他に生年月日や性別を入力することで。良質なデータを蓄積し皆様にお届けすることができます。
-              </Typography>
+              <Grid item xs={12} lg={4}>
+                <Fade duration={3000} bottom>
+                  <Box fontSize="60px" textAlign="center">
+                    <StorageIcon fontSize="inherit" />
+                  </Box>
+                  <BoldTypography variant="h5" align="center" gutterBottom>
+                    - データの蓄積 -
+                  </BoldTypography>
+                  <Typography variant="body1" color="inherit" align="center">
+                    正答数や正答率、試行回数、の他に生年月日や性別を入力することで。良質なデータを蓄積し皆様にお届けすることができます。
+                  </Typography>
+                </Fade>
+              </Grid>
             </Grid>
-          </Grid>
-          <AuthBtns />
-        </Container>
-      </Box>
+            <AuthBtns />
+          </Container>
+        </Box>
+      </Fade>
     </>
   )
 }
@@ -162,95 +184,4 @@ const FirstBox = styled(Box)`
 const BoldTypography = styled(Typography)`
   font-weight: bold !important;
   letter-spacing: 5px !important;
-`
-
-const aaaaasContainer = styled.div`
-  .fade-false {
-    opacity: 0;
-  }
-  .fade-true {
-    opacity: 1;
-    transition: 2s ease;
-  }
-  span {
-    &.red {
-      color: ${Color.red};
-    }
-    &.blue {
-      color: ${Color.blue};
-    }
-  }
-  article {
-    padding: 200px 10%;
-    span,
-    p,
-    h1 {
-      margin: 0;
-      font-weight: bold;
-      letter-spacing: 3px;
-    }
-  }
-  article:first-child {
-    background: linear-gradient(90deg, ${Color.blue} 60%, ${Color.blue2});
-    section {
-      padding: 30px;
-      margin-bottom: 30px;
-    }
-    p,
-    h1 {
-      color: white;
-    }
-    h1 {
-      font-size: 50px;
-    }
-  }
-  article:nth-child(2) {
-    padding: 300px 0;
-    text-align: center;
-    section {
-      h1 {
-        margin-bottom: 50px;
-        text-align: center;
-        font-size: 50px;
-        color: #2b2b2b;
-      }
-      p {
-        margin-bottom: 15px;
-      }
-      span {
-        font-size: 17px;
-      }
-    }
-  }
-  article:last-child {
-    position: relative;
-    background: #2b2b2b;
-    text-align: center;
-    margin-bottom: -30px;
-    section {
-      padding: 30px;
-      p,
-      h1 {
-        color: white;
-      }
-      h1 {
-        margin-bottom: 50px;
-        text-align: center;
-        font-size: 50px;
-      }
-      .row {
-        display: flex;
-        justify-content: space-around;
-      }
-    }
-  }
-  @media screen and (max-width: 1000px) {
-    .subtitle {
-      font-size: 22px;
-    }
-    .row {
-      flex-direction: column;
-      align-items: center;
-    }
-  }
 `
